3325d10cf22a18cbeec2ba12ea0a40bd5472e45f
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-04-04 14:06:07 -0300

    ADD endpoint to return all attendees in event

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,5 +1,6 @@
-export { createEvent as createEvent } from './create-event.js';
-export { registerInEvent as registerInEvent } from './register-in-event.js';
-export { getEvent as getEvent } from './get-event.js';
-export { getAttendeeCredentials as getAttendeeCredentials } from './get-attendee-credentials.js';
-export { checkIn as checkIn } from './check-in.js';
+export { createEvent as createEvent } from "./create-event.js";
+export { registerInEvent as registerInEvent } from "./register-in-event.js";
+export { getEvent as getEvent } from "./get-event.js";
+export { getAttendeeCredentials as getAttendeeCredentials } from "./get-attendee-credentials.js";
+export { checkIn as checkIn } from "./check-in.js";
+export { getEventAttendees as getEventAttendees } from "./get-event-attendees.js";
